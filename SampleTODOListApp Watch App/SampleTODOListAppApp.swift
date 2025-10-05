5fd3280275e3b6ef28763e90d0d844ea770e0c58
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-05 17:30:01 +0200

    Initial Commit

---
+++ SampleTODOListApp Watch App/SampleTODOListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTODOListAppApp.swift
+//  SampleTODOListApp Watch App
+//
+//  Created by Azizbek Asadov on 05.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SampleTODOListApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
